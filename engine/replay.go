ddf8d7fd88f7fe0a3e9c3eae14db5a857a828d55
Mrigank118 <mrigank.11.8@gmail.com>
2025-12-31 16:49:25 +0530

    Implement log replay for crash recovery

---
+++ engine/replay.go
@@ -0,0 +1,45 @@
+package engine
+
+import (
+	"encoding/binary"
+	"io"
+	"os"
+)
+
+func replayLog(e *Engine) error {
+	file, err := os.Open(e.logPath)
+	if err != nil {
+		// No log yet → fresh engine
+		return nil
+	}
+	defer file.Close()
+
+	for {
+		var keyLen uint32
+		if err := binary.Read(file, binary.LittleEndian, &keyLen); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		key := make([]byte, keyLen)
+		if _, err := io.ReadFull(file, key); err != nil {
+			return err
+		}
+
+		var valLen uint32
+		if err := binary.Read(file, binary.LittleEndian, &valLen); err != nil {
+			return err
+		}
+
+		value := make([]byte, valLen)
+		if _, err := io.ReadFull(file, value); err != nil {
+			return err
+		}
+
+		e.index[string(key)] = string(value)
+	}
+
+	return nil
+}
