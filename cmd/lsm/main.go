3b5dcf88baef122d5a8557c8f00c992d30ae0185
Mrigank118 <mrigank.11.8@gmail.com>
2025-12-31 16:48:39 +0530

    Add minimal CLI for SET and GET operations

---
+++ cmd/lsm/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"lsm-storage-engine/engine"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage:")
+		fmt.Println("  SET key value")
+		fmt.Println("  GET key")
+		return
+	}
+
+	db, err := engine.New("store.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch os.Args[1] {
+	case "SET":
+		if len(os.Args) != 4 {
+			log.Fatal("SET requires key and value")
+		}
+		if err := db.Set(os.Args[2], os.Args[3]); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("OK")
+
+	case "GET":
+		val, err := db.Get(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(val)
+
+	default:
+		log.Fatal("unknown command")
+	}
+}
